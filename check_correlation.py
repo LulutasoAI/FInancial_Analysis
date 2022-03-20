3fc5174d412bd2ae52f9f69f0e87574df497eaee
LulutasoAI <hinety3@gmail.com>
2022-03-20 22:38:39 +0900

    Added new function

--- check_correlation.py
+++ check_correlation.py
@@ -3,6 +3,7 @@ from Correlation_Analysis import Correlation_Analysis
 import utils
 from Log_Return_Process import Log_Return_Process
 import pandas as pd
+import numpy as np
 
 class Check_Correlation():
     def __init__(self,start_date= "2015-01-01"):
@@ -12,17 +13,24 @@ class Check_Correlation():
         self.Log_Process = Log_Return_Process()
         self.Correlation_analysis = Correlation_Analysis()
 
-    def fetch_prices(self,symbol1, symbol2):
+    def fetch_prices(self,symbol1 : str, symbol2 : str):
         prices_a = utils.stock_id_to_prices(symbol1,self.start_date)
         prices_b = utils.stock_id_to_prices(symbol2, self.start_date)
         return prices_a,prices_b
     
     def check_pure_correlation(self):
         #usually not usable because gold and stocks both go up thanks to continuous inflation.
+        """
+        I thought it was not usable but it turned out this function is fine.
+        Thanks to the monetary crazy easing The market is distorted.
+        """
         series1,series2 = self.fetch_prices(self.symbol1,self.symbol2)
         self.Correlation_analysis.main(series1,series2)
 
     def convert_value_into_logged_one(self,series : pd.Series) -> pd.Series:
+        """
+        It safely converts pd.Series.values into logged values. 
+        """
         result = pd.Series(dtype='float64')
         index = series.index[1:]
         new_values = self.Log_Process.prices_to_log_return(series)[1:]
@@ -31,8 +39,12 @@ class Check_Correlation():
         return result 
 
 
-    def normalized_correlation(self):
-        
+    def normalized_correlation(self) -> None:
+        """
+        I thought correlation check was wack because they are not normalized.
+        Attempted to normalize the procedure by making them logged.
+        It turned out that it might be totally meaningless.
+        """
         series1,series2 = self.fetch_prices(self.symbol1,self.symbol2)
         series1 = self.convert_value_into_logged_one(series1)
         series2 = self.convert_value_into_logged_one(series2)
@@ -40,18 +52,27 @@ class Check_Correlation():
         
         #series1 = self.Log_Process.
     
-    def Correlation_with_formula(self):
+    def Correlation_with_formula(self) -> float:
         """
-        2つの証券Ａ、Ｂの相関係数の式は
-        （2つの証券の共分散）÷（証券Ａの標準偏差と証券Ｂの標準偏差との積）
-        として定義される。
+        Correlation check with proper formula.
+        but it turned out to be the same result
+        as self.check_pure_correlation.
         """
-        pass 
+        series1,series2 = self.fetch_prices(self.symbol1,self.symbol2)
+        series1, series2 = self.Correlation_analysis.arrange_serieses(series1,series2)
+        covariance = np.cov(series1,series2)[0][1]
+        #print(np.cov(series1,series2)[0][1])
+        denominator = np.std(series1) * np.std(series2)
+        result = covariance/denominator
+        print(result)
+        return result
 
 
 
 
 if __name__ == "__main__":
     checker = Check_Correlation()
-    #checker.main()
-    checker.normalized_correlation()
+    checker.check_pure_correlation()
+    print("devider")
+    #checker.normalized_correlation()
+    checker.Correlation_with_formula()
